077138559045e7f409a78a16f255c7156e59672a
AlthalosV <devanclark5@gmail.com>
2019-03-28 13:18:42 -0500

    Made migrations based on Lab8 sample

--- Final/models.py
+++ Final/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class MyModel(models.Model):
+    fieldOne = models.CharField(max_length=20)
+    fieldTwo = models.IntegerField(default=0)
