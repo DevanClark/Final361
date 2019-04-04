01a7ff3a4c1ba4a5eb58d441ebcb06f2859e19ac
aminPhakhruddin <amin.phakhruddin@gmail.com>
2019-04-04 10:42:53 -0500

    Started on creating a form for our application.

--- Final/views.py
+++ Final/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Final.models import MyModel
+from Final.forms import CommandForm
 
 
 # Create your views here.
@@ -8,3 +9,18 @@ def index(request):
     myQuery = MyModel.objects.all()
     context = {'myObjects': myQuery}
     return render(request, "index.html", context)
+
+def command(request):
+    inputCommand = ""
+
+    if request.method == "POST":
+        #Get the input command from the posted form
+        InputCommandForm = CommandForm(request.POST)
+
+        if InputCommandForm.is_valid():
+            InputCommand = InputCommandForm.cleaned_data['command']
+            #do we call App(InputCommand) here?
+    else:
+        InputCommandForm = CommandForm()
+
+    return render(request, 'command.html', {"command": command})
