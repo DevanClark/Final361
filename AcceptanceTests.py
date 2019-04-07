293578113d903b2906951dd8c5b81c388444ca9d
Jess The Espeon <carpjess@yahoo.com>
2019-04-07 13:39:35 -0500

    Acceptance Tests 4 Now with gitignore

--- AcceptanceTests.py
+++ AcceptanceTests.py
@@ -3,7 +3,7 @@ from User import User
 from App import App
 from Course import Course
 
-class TestApp(TestCase):
+class AcceptanceTests(TestCase):
 
     testUser = User("testUsername", "testPassword", [1, 1, 1, 1])
     brokenTestUser = User("brokenUsername", "brokenPassword", [0, 0, 0, 0])
