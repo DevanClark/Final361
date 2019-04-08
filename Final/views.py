08d810bd70eb84527232080fa2872075362b9f96
AlthalosV <devanclark5@gmail.com>
2019-04-08 13:05:45 -0500

    fixed views for now

--- Final/views.py
+++ Final/views.py
@@ -6,8 +6,11 @@ from App import App
 from Login import Login
 from UserEdits import UserEdits
 from CourseEdit import CourseEdit
+from Final.DjangoInterface import DjangoInterface
 
+a = App(Login(DjangoInterface()), UserEdits(), CourseEdit())
 # Create your views here.
+
 def index(request):
     myQuery = MyModel.objects.all()
     context = {'myObjects': myQuery}
@@ -25,7 +28,6 @@ def command(request):
             InputCommand = InputCommandForm.cleaned_data['command']
             print(InputCommand)
             # do we call App(InputCommand) here?
-            a = App(Login(), UserEdits(), CourseEdit())
             print(a.command(str(InputCommand)))
     else:
         InputCommandForm = CommandForm()
--- Test.py
+++ Test.py
@@ -2,10 +2,13 @@ from App import App
 from Login import Login
 from UserEdits import UserEdits
 from CourseEdit import CourseEdit
+from Final.DjangoInterface import DjangoInterface
+
+
 def main():
     command_string = "true"
     print("True")
-    a = App(Login(), UserEdits(), CourseEdit())
+    a = App(Login(DjangoInterface()), UserEdits(), CourseEdit())
     while command_string != "quit":
         command_string = input("Please enter command: ")
         print(a.command(command_string))
