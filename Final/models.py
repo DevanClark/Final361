37840f84a95181af1044c4827bef44bf85e717af
AlthalosV <devanclark5@gmail.com>
2019-04-09 18:49:34 -0500

    last commit for sprint 1

--- Final/models.py
+++ Final/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class MyModel(models.Model):
     fieldOne = models.CharField(max_length=20)
     fieldTwo = models.IntegerField(default=0)
+
 
 class User(models.Model):
     username = models.CharField(max_length=20)
@@ -11,30 +13,18 @@ class User(models.Model):
     permissions = models.CharField(max_length=20)
 
     address = models.CharField(max_length=200)
-    phonenumber = models.CharField(max_length=200) #was an integer field, might need to change back
+    phonenumber = models.CharField(max_length=200)  # was an integer field, might need to change back
     email = models.CharField(max_length=30)
 
     def UsertoStr(self):
         return "User: " + self.username + " " + self.password + " " + self.permissions + " " \
                + self.address + " " + self.phonenumber + " " + self.email
 
-#Todo - Figure out how to represent the LabList with the Course Model
-#class Lab(models.Model):
-#    labId = models.CharField(max_length = 50)
-#    LabStartTime = models.CharField(max_length = 50)
-#    LabEndTime = models.CharField(max_length = 50)
-
-#    def LabtoStr(self):
-#    return "LabtoStr: " + self.LabId + self.LabStartTime + self.LabEndTime
 
 class Course(models.Model):
-    courseId = models.CharField(max_length = 50)
-    startTime = models.CharField(max_length = 50)
-    endTime = models.CharField(max_length = 50)
-    #LabList = models.ManyToManyField(Lab)
+    courseId = models.CharField(max_length=50)
+    startTime = models.CharField(max_length=50)
+    endTime = models.CharField(max_length=50)
 
     def CoursetoStr(self):
         return "CoursetoStr: " + self.courseId + self.startTime + self.endTime + self.LabList
-
-
-
