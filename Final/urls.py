96497422b4903b95e5ddf635974d3bd129e7aafc
aminPhakhruddin <amin.phakhruddin@gmail.com>
2019-04-18 11:30:25 -0500

    Created a user web interface to allow the admin to edit user information.

--- Final/urls.py
+++ Final/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('command/', views.command),
+    path('editUserAdmin/', views.edituserAdmin),
     # the path for command view
     # add the path to command_result?
     # path ('command_result', views.command_result)
--- Final/views.py
+++ Final/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from Final.models import MyModel
-from Final.forms import CommandForm
+from Final.forms import *
 from App import App
 from Login import Login
 from UserEdits import UserEdits
@@ -30,6 +30,26 @@ def command(request):
             print(a.command(str(InputCommand)))
             cmdResponse = a.command(str(InputCommand))
     else:
-        InputCommandForm = CommandForm() 
+        InputCommandForm = CommandForm()
 
     return render(request, 'commandForm.html', {"cmdResponse": cmdResponse})
+
+def edituserAdmin(request):
+
+    if request.method == "POST":
+        inputEditForm = EditUserForm(request.POST)
+
+        if inputEditForm.is_valid():
+            username = inputEditForm.cleaned_data['username']
+            password = inputEditForm.cleaned_data['password']
+            permissions = inputEditForm.cleaned_data['permissions']
+
+            address = inputEditForm.cleaned_data['address']
+            phonenumber = inputEditForm.cleaned_data['phonenumber']
+            email = inputEditForm.cleaned_data['email']
+            return HttpResponseRedirect('/thanks!/')
+
+    else:
+        inputEditForm = EditUserForm()
+
+    return render(request, 'editUserAdmin.html', {'form': inputEditForm})
