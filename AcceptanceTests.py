a83aec18bd9c2683b01e1499893894e323878d9c
Jess The Espeon <carpjess@yahoo.com>
2019-04-23 12:18:47 -0500

    Updated urls and made more mock acceptance tests

--- AcceptanceTests.py
+++ AcceptanceTests.py
@@ -225,12 +225,16 @@ class TestApp(TestCase):
 
     def test_edituserself(self):
         result1= #page renders correctly
+        result2-X= #correct input results in changed field
+        resultX+1= #incorrect input results in proper string response
 
     def test_landingpage(self):
         result1= #page renders correctly
 
     def test_loginpage(self):
         result1= #page renders correctly
+        result2= #correct input logs someone in
+        result3-4= #incorrect input returns proper string response (user doesn't exist vs username/pass incorrect)
 
     def test_base(self):
         result1= #homepage renders correctly from link
@@ -238,6 +242,8 @@ class TestApp(TestCase):
 
     def test_commandform(self):
         result1= #fields render
+        result2-X= #correct input returns proper output (CommandFormResult)
+        resultX+1-Y= #incorrect input returns proper string output
 
     def test_commandformresult(self):
-        result1= #fields render
+        result1= #page renders correctly
--- Final/urls.py
+++ Final/urls.py
@@ -5,7 +5,9 @@ from Final.views import LoginClass
 urlpatterns = [
     path('command/', views.command),
     path('loginpage/', LoginClass.as_view()),
-    path('landingpage/', views.command)
+    path('landingpage/', views.command),
+    path('createuser/', views.CreateUserClass.as_view()),
+    path('edituserself/', views.EditUserSelfClass.as_view()),
     # the path for command view
     # add the path to command_result?
     # path ('command_result', views.command_result)
